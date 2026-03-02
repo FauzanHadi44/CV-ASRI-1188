07c6e206490a8b0008a0f5243215822afd220041
fauzanhadi44 <fauzanhadi097@gmail.com>
2026-03-02 20:28:23 +0700

    fix divider footer

--- src/components/section/hubungi/ContactFormSection.tsx
+++ src/components/section/hubungi/ContactFormSection.tsx
@@ -50,7 +50,7 @@ export default function ContactFormSection() {
     };
 
     return (
-        <section className="py-20 bg-brand-light relative overflow-hidden">
+        <section className="py-20 pb-40 bg-brand-light relative overflow-hidden">
             <div className="absolute bottom-0 right-0 w-[500px] h-[500px] bg-brand-navy/5 rounded-full blur-[120px] translate-x-1/2 translate-y-1/2 pointer-events-none" />
 
             <div className="container mx-auto px-6 relative z-10">
@@ -199,6 +199,19 @@ export default function ContactFormSection() {
                         </div>
                     </div>
                 </div>
+            </div>
+            <div className="absolute bottom-0 left-0 w-full overflow-hidden leading-none rotate-180">
+                <svg
+                    className="relative block w-full h-[60px] md:h-[120px] drop-shadow-lg"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 1200 120"
+                    preserveAspectRatio="none"
+                >
+                    <path
+                        d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+                        className="fill-white"
+                    ></path>
+                </svg>
             </div>
         </section>
     );
